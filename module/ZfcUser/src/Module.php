bcfa6312d1ae673d5a351bf74e5398fa59dcb2f4
rarog <rarogit@gmail.com>
2020-03-28 02:08:56 +0100

    Making Module PSR-4 compliant.

--- module/ZfcUser/src/Module.php
+++ module/ZfcUser/src/Module.php
@@ -2,84 +2,128 @@
 
 namespace ZfcUser;
 
+use Laminas\Db\Adapter\Adapter;
+use Laminas\Hydrator\ClassMethodsHydrator;
 use Laminas\ModuleManager\Feature\ConfigProviderInterface;
 use Laminas\ModuleManager\Feature\ControllerPluginProviderInterface;
 use Laminas\ModuleManager\Feature\ControllerProviderInterface;
 use Laminas\ModuleManager\Feature\ServiceProviderInterface;
+use Laminas\ModuleManager\Feature\ViewHelperProviderInterface;
+use ZfcUser\Authentication\Adapter\AdapterChain;
+use ZfcUser\Authentication\Adapter\AdapterChainServiceFactory;
+use ZfcUser\Authentication\Adapter\Db as AdapterDb;
+use ZfcUser\Authentication\Storage\Db as StorageDb;
+use ZfcUser\Factory\AuthenticationService;
+use ZfcUser\Factory\UserHydrator;
+use ZfcUser\Factory\Authentication\Adapter\DbFactory as AdapterDbFactory;
+use ZfcUser\Factory\Authentication\Storage\DbFactory as StorageDbFactory;
+use ZfcUser\Factory\Controller\RedirectCallbackFactory;
+use ZfcUser\Factory\Controller\UserControllerFactory;
+use ZfcUser\Factory\Controller\Plugin\ZfcUserAuthentication;
+use ZfcUser\Factory\Form\ChangeEmail;
+use ZfcUser\Factory\Form\ChangePassword;
+use ZfcUser\Factory\Form\Login;
+use ZfcUser\Factory\Form\Register;
+use ZfcUser\Factory\Mapper\User;
+use ZfcUser\Factory\Options\ModuleOptions;
+use ZfcUser\Factory\Service\UserFactory;
+use ZfcUser\Factory\View\Helper\ZfcUserDisplayName;
+use ZfcUser\Factory\View\Helper\ZfcUserIdentity;
+use ZfcUser\Factory\View\Helper\ZfcUserLoginWidget;
 
 class Module implements
     ControllerProviderInterface,
     ControllerPluginProviderInterface,
     ConfigProviderInterface,
-    ServiceProviderInterface
+    ServiceProviderInterface,
+    ViewHelperProviderInterface
 {
+    /**
+     * {@inheritDoc}
+     * @see \Laminas\ModuleManager\Feature\ConfigProviderInterface::getConfig()
+     */
     public function getConfig($env = null)
     {
         return include __DIR__ . '/../config/module.config.php';
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Laminas\ModuleManager\Feature\ControllerPluginProviderInterface::getControllerPluginConfig()
+     */
     public function getControllerPluginConfig()
     {
-        return array(
-            'factories' => array(
-                'zfcUserAuthentication' => \ZfcUser\Factory\Controller\Plugin\ZfcUserAuthentication::class,
-            ),
-        );
+        return [
+            'factories' => [
+                'zfcUserAuthentication' => ZfcUserAuthentication::class,
+            ],
+        ];
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Laminas\ModuleManager\Feature\ControllerProviderInterface::getControllerConfig()
+     */
     public function getControllerConfig()
     {
-        return array(
-            'factories' => array(
-                'zfcuser' => \ZfcUser\Factory\Controller\UserControllerFactory::class,
-            ),
-        );
+        return [
+            'factories' => [
+                'zfcuser' => UserControllerFactory::class,
+            ],
+        ];
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Laminas\ModuleManager\Feature\ViewHelperProviderInterface::getViewHelperConfig()
+     */
     public function getViewHelperConfig()
     {
-        return array(
-            'factories' => array(
-                'zfcUserDisplayName' => \ZfcUser\Factory\View\Helper\ZfcUserDisplayName::class,
-                'zfcUserIdentity' => \ZfcUser\Factory\View\Helper\ZfcUserIdentity::class,
-                'zfcUserLoginWidget' => \ZfcUser\Factory\View\Helper\ZfcUserLoginWidget::class,
-            ),
-        );
-
+        return [
+            'factories' => [
+                'zfcUserDisplayName' => ZfcUserDisplayName::class,
+                'zfcUserIdentity' => ZfcUserIdentity::class,
+                'zfcUserLoginWidget' => ZfcUserLoginWidget::class,
+            ],
+        ];
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Laminas\ModuleManager\Feature\ServiceProviderInterface::getServiceConfig()
+     */
     public function getServiceConfig()
     {
-        return array(
-            'aliases' => array(
-                'zfcuser_zend_db_adapter' => \Laminas\Db\Adapter\Adapter::class,
-            ),
-            'invokables' => array(
-                'zfcuser_register_form_hydrator' => \Laminas\Hydrator\ClassMethods::class,
-            ),
-            'factories' => array(
-                'zfcuser_redirect_callback' => \ZfcUser\Factory\Controller\RedirectCallbackFactory::class,
-                'zfcuser_module_options' => \ZfcUser\Factory\Options\ModuleOptions::class,
-                'ZfcUser\Authentication\Adapter\AdapterChain' => \ZfcUser\Authentication\Adapter\AdapterChainServiceFactory::class,
+        return [
+            'aliases' => [
+                'zfcuser_zend_db_adapter' => Adapter::class,
+            ],
+            'invokables' => [
+                'zfcuser_register_form_hydrator' => ClassMethodsHydrator::class,
+            ],
+            'factories' => [
+                'zfcuser_redirect_callback' => RedirectCallbackFactory::class,
+                'zfcuser_module_options' => ModuleOptions::class,
+                AdapterChain::class => AdapterChainServiceFactory::class,
 
                 // We alias this one because it's ZfcUser's instance of
                 // Laminas\Authentication\AuthenticationService. We don't want to
-                // hog the FQCN service alias for a Zend\* class.
-                'zfcuser_auth_service' => \ZfcUser\Factory\AuthenticationService::class,
+                // hog the FQCN service alias for a Laminas\* class.
+                'zfcuser_auth_service' => AuthenticationService::class,
 
-                'zfcuser_user_hydrator' => \ZfcUser\Factory\UserHydrator::class,
-                'zfcuser_user_mapper' => \ZfcUser\Factory\Mapper\User::class,
+                'zfcuser_user_hydrator' => UserHydrator::class,
+                'zfcuser_user_mapper' => User::class,
 
-                'zfcuser_login_form' => \ZfcUser\Factory\Form\Login::class,
-                'zfcuser_register_form' => \ZfcUser\Factory\Form\Register::class,
-                'zfcuser_change_password_form' => \ZfcUser\Factory\Form\ChangePassword::class,
-                'zfcuser_change_email_form' => \ZfcUser\Factory\Form\ChangeEmail::class,
+                'zfcuser_login_form' => Login::class,
+                'zfcuser_register_form' => Register::class,
+                'zfcuser_change_password_form' => ChangePassword::class,
+                'zfcuser_change_email_form' => ChangeEmail::class,
 
-                'ZfcUser\Authentication\Adapter\Db' => \ZfcUser\Factory\Authentication\Adapter\DbFactory::class,
-                'ZfcUser\Authentication\Storage\Db' => \ZfcUser\Factory\Authentication\Storage\DbFactory::class,
+                AdapterDb::class => AdapterDbFactory::class,
+                StorageDb::class => StorageDbFactory::class,
 
-                'zfcuser_user_service' => \ZfcUser\Factory\Service\UserFactory::class,
-            ),
-        );
+                'zfcuser_user_service' => UserFactory::class,
+            ],
+        ];
     }
 }
